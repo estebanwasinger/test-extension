7024ec4eeed215824a6d56f4f3179853731df1a6
estebanwasinger <estebanwasinger@gmail.com>
2017-03-13 12:47:46 -0300

    more updates

--- src/main/java/org/mule/extension/some/api/TestExtension.java
+++ src/main/java/org/mule/extension/some/api/TestExtension.java
@@ -5,10 +5,12 @@ import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.Export;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
+import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 
 @Extension(name = "Test Extension", vendor = "MuleSoft Inc.", category = Category.SELECT)
 @Configurations({ConfigOne.class})
 @ConnectionProviders(ConnProviderOne.class)
 @Export(classes = ConfigOne.class)
+@Xml(namespace = "metadata")
 public class TestExtension {
 }
